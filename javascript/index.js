a6416222e6fe5fe99d4c52a724288e686c1c0a1a
Maximiliano Huerta <maxihuerta2@yahoo.com.ar>
2024-10-01 22:21:19 -0300

    Entrega coder

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+let input;
+let total = 0;
+
+while (true) {
+  input = prompt("ingrese el valor del producto o terminar compra");
+  if (input.toLowerCase() === "terminar") {
+    break;
+  }
+
+  let valorProducto = parseFloat(input);
+  
+
+
+}
